82e1bee83643fc9bb4ef26048a791f2ba4fddee2
xiaozhujun <xiao520@163.com>
2015-01-29 18:53:32 +0800

    对程序中一些错误进行修改  以及上传数据库脚本

--- funfamental/funfamental-dataSource/src/main/java/com/huawei/smart/dataSource/DbcpFactoryBean.java
+++ funfamental/funfamental-dataSource/src/main/java/com/huawei/smart/dataSource/DbcpFactoryBean.java
@@ -1,6 +1,7 @@
 package com.huawei.smart.dataSource;
 
-import config.FundamentalConfigProvider;
+
+import com.huawei.smart.config.FundamentalConfigProvider;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
@@ -12,109 +13,112 @@ import java.util.Map;
 import java.util.Properties;
 
 public class DbcpFactoryBean implements FactoryBean<BasicDataSource>,
-		DisposableBean, InitializingBean {
+        DisposableBean, InitializingBean {
+    private BasicDataSource ds;
+    private Properties properties;
+    private String dbname;
 
-
-	private BasicDataSource ds;
-	private Properties properties;
-	private String dbname;
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
+    @Override
+    public void afterPropertiesSet() throws Exception {
         setProperties(FundamentalConfigProvider.getProp());
-	}
+    }
 
-	public BasicDataSource getObject() {
-		ds = new BasicDataSource();
+    public BasicDataSource getObject() {
+        ds = new BasicDataSource();
 
-		this.tryToSetProperties();
+        this.tryToSetProperties();
 
-		return ds;
-	}
+        return ds;
+    }
 
-	public void destroy() {
-		if (ds == null) {
-			return;
-		}
-		try {
-			ds.close();
-			ds = null;
-		} catch (Exception ex) {
-            System.out.println("close dbcp error:"+ex);
-		}
-	}
+    public void destroy() {
+        if (ds == null) {
+            return;
+        }
+        try {
+            ds.close();
+            ds = null;
+        } catch (Exception ex) {
+            System.out.println("close dbcp error:"+ ex);
+        }
+    }
 
-	public Class<?> getObjectType() {
-		return BasicDataSource.class;
-	}
+    public Class<?> getObjectType() {
+        return BasicDataSource.class;
+    }
 
-	public boolean isSingleton() {
-		return true;
-	}
+    public boolean isSingleton() {
+        return true;
+    }
 
-	public void setProperties(Properties properties) {
-		this.properties = properties;
-	}
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
 
-	public void setDbname(String dbname) {
+    public void setDbname(String dbname) {
         this.dbname = dbname;
     }
 
-	protected void tryToSetProperties() {
-		String configPrefix = "dbcp.";
-		if (dbname != null && dbname.trim().length() > 0) {
-			configPrefix = configPrefix + dbname + ".";
-            System.out.println("configPrefix:"+configPrefix);
-		}
-		for (Map.Entry<Object, Object> entry : properties.entrySet()) {
-			String key = (String) entry.getKey();
-			String value = (String) entry.getValue();
-            System.out.println("error to set property : key : " + key+ ", value : " + value);
+    protected void tryToSetProperties() {
+        String configPrefix = "dbcp.";
+        if (dbname != null && dbname.trim().length() > 0) {
+            configPrefix = configPrefix + dbname + ".";
+        }
+
+        if (properties == null) {
+            throw new IllegalArgumentException(
+                    "there is no dbcp properties setting.");
+        }
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            String key = (String) entry.getKey();
+            String value = (String) entry.getValue();
+
             if (!key.startsWith(configPrefix)) {
-				continue;
-			}
-			String propertyName = key.substring(configPrefix.length());
-			try {
-				tryToSetProperty(propertyName, value);
-			} catch (Exception ex) {
+                continue;
+            }
 
-			}
-		}
-	}
+            String propertyName = key.substring(configPrefix.length());
+            try {
+                tryToSetProperty(propertyName, value);
+            } catch (Exception ex) {
 
-	protected void tryToSetProperty(String propertyName, String propertyValue)
-			throws Exception {
-		String setterName = "set" + propertyName.substring(0, 1).toUpperCase(Locale.CHINA)
-				+ propertyName.substring(1);
-		Method[] methods = BasicDataSource.class.getMethods();
-		for (Method method : methods) {
-			if (!method.getName().equals(setterName)) {
-				continue;
-			}
-			Class<?>[] parameterTypes = method.getParameterTypes();
-			if (parameterTypes.length == 1) {
-				Class<?> parameterType = parameterTypes[0];
-				this.invokeSetValue(method, parameterType, propertyValue);
-			}
-		}
-	}
+            }
+        }
+    }
 
-	private void invokeSetValue(Method method, Class<?> parameterType,
-			String propertyValue) throws Exception {
+    protected void tryToSetProperty(String propertyName, String propertyValue)
+            throws Exception {
+        String setterName = "set" + propertyName.substring(0, 1).toUpperCase(Locale.CHINA)
+                + propertyName.substring(1);
+        Method[] methods = BasicDataSource.class.getMethods();
+        for (Method method : methods) {
+            if (!method.getName().equals(setterName)) {
+                continue;
+            }
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            if (parameterTypes.length == 1) {
+                Class<?> parameterType = parameterTypes[0];
+                this.invokeSetValue(method, parameterType, propertyValue);
+            }
+        }
+    }
+
+    private void invokeSetValue(Method method, Class<?> parameterType,
+                                String propertyValue) throws Exception {
         System.out.println("name :"+method.getName()+" value :"+propertyValue);
-		if (parameterType == String.class) {
-			method.invoke(ds, propertyValue);
-		} else if (parameterType == Integer.class || parameterType == int.class) {
-			method.invoke(ds, Integer.parseInt(propertyValue));
-		} else if (parameterType == Long.class || parameterType == long.class) {
-			method.invoke(ds, Long.parseLong(propertyValue));
-		} else if (parameterType == Boolean.class
-				|| parameterType == boolean.class) {
-			method.invoke(ds, Boolean.valueOf(propertyValue));
-		} else {
-            System.out.println("cannot process parameterType : [" + parameterType
-					+ "]");
-		}
-	}
+        if (parameterType == String.class) {
+            method.invoke(ds, propertyValue);
+        } else if (parameterType == Integer.class || parameterType == int.class) {
+            method.invoke(ds, Integer.parseInt(propertyValue));
+        } else if (parameterType == Long.class || parameterType == long.class) {
+            method.invoke(ds, Long.parseLong(propertyValue));
+        } else if (parameterType == Boolean.class
+                || parameterType == boolean.class) {
+            method.invoke(ds, Boolean.valueOf(propertyValue));
+        } else {
+           System.out.println("cannot process parameterType : [" + parameterType
+                    + "]");
+        }
+    }
 
 }
