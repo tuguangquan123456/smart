f1bf2f507e42b27f66a6ba100c1e6455fcb2a8b8
xiaozhujun <xiao520@163.com>
2015-02-08 18:55:49 +0800

    增加图书管理模块的一些功能的后台代码

---
+++ HR-CR/HR-CR-business/HR-CR-business-book/src/main/java/com/huawei/smart/business/book/entity/BookBr.java
@@ -0,0 +1,75 @@
+package com.huawei.smart.business.book.entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tgq
+ * Date: 15-2-8
+ * Time: 下午4:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookBr {
+    private long BookID;
+    private String BookName;
+    private String  Btime;
+    private String  Rtime;
+    private String   LendID;
+    private String  Cangku;
+    private long ID;
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public long getID() {
+
+        return ID;
+    }
+
+    public long getBookID() {
+        return BookID;
+    }
+
+    public String getBookName() {
+        return BookName;
+    }
+
+    public String getBtime() {
+        return Btime;
+    }
+
+    public String getRtime() {
+        return Rtime;
+    }
+
+    public String getLendID() {
+        return LendID;
+    }
+
+    public String getCangku() {
+        return Cangku;
+    }
+
+    public void setBookID(long bookID) {
+        BookID = bookID;
+    }
+
+    public void setBookName(String bookName) {
+        BookName = bookName;
+    }
+
+    public void setRtime(String rtime) {
+        Rtime = rtime;
+    }
+
+    public void setBtime(String btime) {
+        Btime = btime;
+    }
+
+    public void setLendID(String lendID) {
+        LendID = lendID;
+    }
+
+    public void setCangku(String cangku) {
+        Cangku = cangku;
+    }
+}
